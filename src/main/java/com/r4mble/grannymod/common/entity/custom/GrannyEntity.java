48a09298368a2accee286735f119c73f124fceb2
R4mBLe <misharad2008@gmail.com>
2024-02-18 20:39:56 +0300

    update entity atttributes

--- src/main/java/com/r4mble/grannymod/common/entity/custom/GrannyEntity.java
+++ src/main/java/com/r4mble/grannymod/common/entity/custom/GrannyEntity.java
@@ -3,14 +3,11 @@ package com.r4mble.grannymod.common.entity.custom;
 
 import com.r4mble.grannymod.common.item.ModItems;
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.MobEntity;
 import net.minecraft.entity.ai.attributes.AttributeModifierMap;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
-import net.minecraft.entity.ai.goal.WaterAvoidingRandomWalkingGoal;
 import net.minecraft.entity.ai.goal.ZombieAttackGoal;
 import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.monster.ZombieEntity;
@@ -18,9 +15,6 @@ import net.minecraft.entity.passive.AnimalEntity;
 import net.minecraft.entity.passive.IronGolemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.potion.EffectInstance;
-import net.minecraft.potion.Effects;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.SoundEvents;
@@ -35,10 +29,10 @@ public class GrannyEntity extends ZombieEntity {
     public static AttributeModifierMap.MutableAttribute setCustomAttributes() {
         return MobEntity.func_233666_p_()
                 .createMutableAttribute(Attributes.MAX_HEALTH, 100.00D)
-                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.5D)
+                .createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2D)
                 .createMutableAttribute(Attributes.ATTACK_DAMAGE, 10.00D)
                 .createMutableAttribute(Attributes.ARMOR, 5.00D)
-                .createMutableAttribute(Attributes.ATTACK_SPEED, 5.00D)
+                .createMutableAttribute(Attributes.ATTACK_SPEED, 3.00D)
                 .createMutableAttribute(Attributes.FOLLOW_RANGE, 24.00D);
     }
 
@@ -56,9 +50,13 @@ public class GrannyEntity extends ZombieEntity {
         super.dropSpecialItems(p_213333_1_, p_213333_2_, p_213333_3_);
         ItemStack sword = ModItems.GRANNY_SWORD.get().getDefaultInstance();
         this.entityDropItem(sword);
-        this.entityDropItem(Items.DIAMOND, world.rand.nextInt(32));
     }
 
+    @Override
+    public boolean isDrowning()
+    {
+        return false;
+    }
     @Override
     protected int getExperiencePoints(PlayerEntity player)
     {
@@ -88,19 +86,5 @@ public class GrannyEntity extends ZombieEntity {
     protected void playStepSound(BlockPos pos, BlockState blockIn)
     {
         this.playSound(SoundEvents.ENTITY_HOGLIN_STEP, 0.20F, 0.5F);
-    }
-
-    @Override
-    public boolean attackEntityAsMob(Entity entityIn) {
-        if (!super.attackEntityAsMob(entityIn)) {
-            return false;
-        } else {
-            if (entityIn instanceof LivingEntity) {
-                ((LivingEntity)entityIn).addPotionEffect(new EffectInstance(Effects.SLOWNESS, 200,3));
-                ((LivingEntity)entityIn).addPotionEffect(new EffectInstance(Effects.WEAKNESS, 200));
-                ((LivingEntity)entityIn).addPotionEffect(new EffectInstance(Effects.NAUSEA, 200));
-            }
-            return true;
-        }
     }
 }
