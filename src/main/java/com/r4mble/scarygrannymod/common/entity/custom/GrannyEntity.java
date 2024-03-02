739e9a73581b1105e8de3d80507590b3043f9973
R4mBLe <misharad2008@gmail.com>
2024-03-03 02:34:21 +0300

    add custom sounds, fix project bugs

--- src/main/java/com/r4mble/scarygrannymod/common/entity/custom/GrannyEntity.java
+++ src/main/java/com/r4mble/scarygrannymod/common/entity/custom/GrannyEntity.java
@@ -3,11 +3,10 @@ package com.r4mble.scarygrannymod.common.entity.custom;
 
 import com.r4mble.scarygrannymod.common.item.ModItems;
 import com.r4mble.scarygrannymod.common.util.ModSounds;
-import net.minecraft.block.BlockState;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.util.SoundEvents;
-import net.minecraft.util.math.BlockPos;
+import net.minecraft.core.BlockPos;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
@@ -19,10 +18,12 @@ import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.state.BlockState;
 
 public class GrannyEntity extends Monster {
     public GrannyEntity(EntityType<? extends Monster> type, Level worldIn) {
         super(type, worldIn);
+        this.xpReward = 300;
     }
 
     public static AttributeSupplier setCustomAttributes() {
@@ -54,11 +55,6 @@ public class GrannyEntity extends Monster {
         ItemStack sword = ModItems.GRANNY_BAT.get().getDefaultInstance();
         sword.setDamageValue(random.nextInt(100));
         this.spawnAtLocation(sword);
-    }
-
-    public int getExperienceReward(Player p_34322_) {
-        this.xpReward = 3 + this.level.random.nextInt(300);
-        return super.getExperienceReward(p_34322_);
     }
 
     @Override
